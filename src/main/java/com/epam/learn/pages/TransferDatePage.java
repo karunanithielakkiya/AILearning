8193d0c3b9647e1b06ad95b94c52a8ea53bb6e11
karunanithielakkiya <67217077+karunanithielakkiya@users.noreply.github.com>
2025-01-23 12:30:53 +0530

    Create src/main/java/com/epam/learn/pages/TransferDatePage.java

---
+++ src/main/java/com/epam/learn/pages/TransferDatePage.java
@@ -0,0 +1,38 @@
+package com.epam.learn.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class TransferDatePage {
+
+    WebDriver driver;
+
+    @FindBy(id = "transferDateInput")
+    private WebElement transferDateInput;
+
+    @FindBy(id = "confirmationMessage")
+    private WebElement confirmationMessage;
+
+    @FindBy(id = "errorMessage")
+    private WebElement errorMessage;
+
+    public TransferDatePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void enterTransferDate(String date) {
+        transferDateInput.clear();
+        transferDateInput.sendKeys(date);
+    }
+
+    public String getConfirmationMessage() {
+        return confirmationMessage.getText();
+    }
+
+    public String getErrorMessage() {
+        return errorMessage.getText();
+    }
+}
